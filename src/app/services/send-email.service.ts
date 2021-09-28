9a8b9aeccc4db2ec07862399fcd22ab5a617f0e5
Juan Hoyos <jcabarique@uninorte.edu.co>
2021-09-28 13:37:06 -0500

    se desplego en firebase

--- src/app/services/send-email.service.ts
+++ src/app/services/send-email.service.ts
@@ -16,17 +16,17 @@ export class SendEmailService {
 
   //Envia los archivos a la base de datos
   sendFiles(data) {
-    return this.http.post('https://tranquil-dusk-36230.herokuapp.com/', data);
+    return this.http.post('https://blooming-eyrie-24542.herokuapp.com/', data);
   }
 
   //Obtiene los correos enviados y sus archivos
   getEmails() {
-    return this.http.get('https://tranquil-dusk-36230.herokuapp.com/emails', {headers: {'access-token': this.auth.userToken}});
+    return this.http.get('https://blooming-eyrie-24542.herokuapp.com/emails', {headers: {'access-token': this.auth.userToken}});
   }
 
   descargarZip(ubicacion: string[], nombre: string) {
       this.loading = true;
-      this.http.post<Blob>(`https://tranquil-dusk-36230.herokuapp.com/descargaZip`, {ubicacion: ubicacion},  {headers: {'access-token': this.auth.userToken}, observe: 'response', responseType: 'blob' as 'json'})
+      this.http.post<Blob>(`https://blooming-eyrie-24542.herokuapp.com/descargaZip`, {ubicacion: ubicacion},  {headers: {'access-token': this.auth.userToken}, observe: 'response', responseType: 'blob' as 'json'})
               .subscribe((data) => {
               
         //Libreria que ayuda en las descargas de archivos        
@@ -39,18 +39,18 @@ export class SendEmailService {
 
   //Se encarga de marcar el leido y mandar el actualziado al backend
   marcarLeido(id, leido) {
-    this.http.post('https://tranquil-dusk-36230.herokuapp.com/marcarLeido', {id, leido: leido}, {headers: {'access-token': this.auth.userToken}}).subscribe(res => console.log(res))
+    this.http.post('https://blooming-eyrie-24542.herokuapp.com/marcarLeido', {id, leido: leido}, {headers: {'access-token': this.auth.userToken}}).subscribe(res => console.log(res))
   }
 
 
   //Se encarga de enviar el correo al backend
   sendEmail(data) {
-    return this.http.post('https://tranquil-dusk-36230.herokuapp.com/email', data);
+    return this.http.post('https://blooming-eyrie-24542.herokuapp.com/email', data);
   }
 
 
   login(usuario) {
-    return this.http.post('https://tranquil-dusk-36230.herokuapp.com/login', usuario)
+    return this.http.post('https://blooming-eyrie-24542.herokuapp.com/login', usuario)
                 .pipe(map(res => {
                   if(res['token']) this.auth.guardarToken(res['token']);
                   return res;
